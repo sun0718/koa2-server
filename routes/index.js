c205c491783c2de7084423f70f9537255abc8912
sun0718 <sun0718@qq.com>
2019-02-25 17:11:52 +0800

    test jenkins

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', routerFun.index)
 
 router.get('/string', async (ctx, next) => {
   console.log(ctx.request)
-  ctx.body = 'koa2 string123'
+  ctx.body = 'koa2 string1234'
 })
 
 router.get('/json', async (ctx, next) => {
