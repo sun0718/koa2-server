5af0136758bfd3c4b827a807b763a0b1b7336b0b
sun0718 <sun0718@qq.com>
2019-03-04 16:27:33 +0800

    111

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const bodyparser = require('koa-bodyparser')
 // 输出请求日志的功能
 const logger = require('koa-logger')
 // 跨域设置
-const cors = require('koa2-cors');
+// const cors = require('koa2-cors');
 //node项目配置文件的管理
 const config = require('config');
 
@@ -21,14 +21,14 @@ const users = require('./routes/users')
 // 实例化Koa
 const app = new Koa()
 
-app.use(cors({
-  origin:['http://203.195.175.18'],  //允许这个域名访问
-  methods:['GET','POST','PUT','DELETE','OPTION'], // 只允许http请求方式
-  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-  maxAge: 5,
-  credentials: true,
-  allowHeaders: ['Content-Type', 'Authorization', 'Accept']  // 只允许带着两种请求头的链接访问
-}))
+// app.use(cors({
+//   origin:['http://203.195.175.18'],  //允许这个域名访问
+//   methods:['GET','POST','PUT','DELETE','OPTION'], // 只允许http请求方式
+//   exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+//   maxAge: 5,
+//   credentials: true,
+//   allowHeaders: ['Content-Type', 'Authorization', 'Accept']  // 只允许带着两种请求头的链接访问
+// }))
 
 // error handler
 onerror(app)
